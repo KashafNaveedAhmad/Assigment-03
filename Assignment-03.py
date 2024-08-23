0b3357fcfa3b2be085e7e29d9e95ca2060b8a7de
KashafNaveedAhmad <kashafnaveed329@gmail.com>
2024-08-23 13:14:23 +0500

    AGE

--- Assignment-03.py
+++ Assignment-03.py
@@ -1,2 +1,13 @@
-# Assignment-03
 
+import math
+import time
+
+# Assignment-03
+print("Let's start the assignment!!")
+
+# Age using current year and birth year
+print("\nWe are going to calculate your age.\nBelow:")
+birth_year=int(input("Enter your birth year:"))
+current_year=int(time.strftime('%Y'))
+age=current_year-birth_year
+print("Your age based on your birth year is:",age,"years old")
