6d35ccffc0d9b399782e99eb7cdda31d9304d0b1
KashafNaveedAhmad <kashafnaveed329@gmail.com>
2024-08-23 13:17:47 +0500

    AREA OF A CIRCLE

--- Assignment-03.py
+++ Assignment-03.py
@@ -21,3 +21,14 @@ width=int(input("Enter the width:"))
 area=length*width
 print("The area of rectangle is:",area)
 
+# Write a program that calculates the area of a circle.
+# Area of circle
+print("We are going to calculate the 'Area of Circle' :\n Below:")
+rad=int(input("Enter the radius to calculate the area of circle:"))
+area_of_circle=((math.pi)*(rad**2))
+print("The area of circle is:",area_of_circle)
+
+
+
+
+
