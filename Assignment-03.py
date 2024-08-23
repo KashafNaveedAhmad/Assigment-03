b9d3bec286868f46aa71c18d454fadcce812174a
KashafNaveedAhmad <kashafnaveed329@gmail.com>
2024-08-23 13:27:39 +0500

    AREA OF A CUBE

--- Assignment-03.py
+++ Assignment-03.py
@@ -14,19 +14,26 @@ age=current_year-birth_year
 print("Your age based on your birth year is:",age,"years old")
 
 # Write a program that calculates the area of a rectangle using length and width variables.
-# Area of recatangle
-print("\nWe are going to calculates the 'Area of a rectangle'. \nBelow:")
+# Area of recatangle(length*width)
+print("\nLet's calculates the 'Area of a rectangle'.\nBelow:")
 length=float(input("Enter the lenght:"))
 width=int(input("Enter the width:"))
 area=length*width
 print("The area of rectangle is:",area)
 
 # Write a program that calculates the area of a circle.
-# Area of circle
-print("We are going to calculate the 'Area of Circle' :\n Below:")
-rad=int(input("Enter the radius to calculate the area of circle:"))
-area_of_circle=((math.pi)*(rad**2))
+# Area of circle(((math.pi)*(radius**2)))
+print("\nLet's calculate the 'Area of Circle'.\nBelow:")
+radius_of_circle=int(input("Enter the radius:"))
+area_of_circle=((math.pi)*(radius_of_circle**2))
 print("The area of circle is:",area_of_circle)
+
+# Write a program that calculates the area of the cube.
+# Area of circle(6(a^2))
+print("\nLet's calculate the 'Area of Cube'.\nBelow:")
+side_of_cube=int(input("Enter the it's side:"))
+area_of_cube=(6*(side_of_cube**2))
+print("The area of cube is:",area_of_cube)
 
 
 
