b9f995fd29eae20d420fdbe2ca3936a8ea0f823c
KashafNaveedAhmad <kashafnaveed329@gmail.com>
2024-08-23 14:18:28 +0500

    PERCENTAGE

--- Assignment-03.py
+++ Assignment-03.py
@@ -13,6 +13,7 @@ current_year=int(time.strftime('%Y'))
 age=current_year-birth_year
 print("Your age based on your birth year is:",age,"years old.")
 
+
 # Write a program that calculates the area of a rectangle using length and width variables.
 # Area of recatangle(length*width)
 print("\nLet's calculates the 'Area of a rectangle'.\nBelow:")
@@ -21,12 +22,14 @@ width=float(input("Enter the width:"))
 area=length*width
 print("The area of rectangle is:",area)
 
+
 # Write a program that calculates the area of a circle.
 # Area of circle (Formula:(math.pi)*(radius**2))
 print("\nLet's calculate the 'Area of Circle'.\nBelow:")
 radius_of_circle=float(input("Enter the radius:"))
 area_of_circle=((math.pi)*(radius_of_circle**2))
 print("The area of circle is:",area_of_circle)
+
 
 # Write a program that calculates the area of the cube.
 # Area of circle (Formula:6(a^2))
@@ -35,12 +38,14 @@ side_of_cube=int(input("Enter the it's side:"))
 area_of_cube=(6*(side_of_cube**2))
 print("The area of cube is:",area_of_cube)
 
+
 # Create a program that converts a temperature from Fahrenheit to Celsius and vice versa using a variable.
 # Fahrenheit to Celsius (Formula:(5/9)*(F-32))
 print("\nWe are going to convert 'Fahrenheit' into 'Celsius': \nBelow:")
 input_fahrenheit=float(input("Enter the temperature in 'Fahrenheit':"))
 celsius=float((5/9)*(input_fahrenheit-32))
 print("The converted temperature in 'Celsius' is:",celsius,"°C")
+
 
 # Celsius to Fahrenheit (Formula:((9/5)*C)+32))
 print("\nWe are going to convert 'Celsius' into 'Fahrenheit': \nBelow:")
@@ -61,4 +66,12 @@ print("\nTo calculate number of minutes into seconds.\nEnter values below:")
 min=float(input("Enter the minutes to be converted into seconds:"))
 sec=float(min*60)
 print("Based on the minutes the seconds are :",sec)
-
+ 
+ 
+# Write a program that calculates the percentage.
+# Percentge calculation
+print("\nLet's calculate the percentage.\nBelow:")
+num1=int(input("Enter the obtained number:"))
+num2=int(input("Enter the total number:"))
+percentage=(num1/num2)*100
+print("The percentage is:",percentage,'%')
