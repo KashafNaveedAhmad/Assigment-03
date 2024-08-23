c86f0fa1cb1acf9aa737f4f324bbfeb0a8e9b1ae
KashafNaveedAhmad <kashafnaveed329@gmail.com>
2024-08-23 13:17:22 +0500

    AREA OF RECTANGLE

--- Assignment-03.py
+++ Assignment-03.py
@@ -5,9 +5,19 @@ import time
 # Assignment-03
 print("Let's start the assignment!!")
 
+# Calculate your age based on the current year and your birth year.
 # Age using current year and birth year
 print("\nWe are going to calculate your age.\nBelow:")
 birth_year=int(input("Enter your birth year:"))
 current_year=int(time.strftime('%Y'))
 age=current_year-birth_year
 print("Your age based on your birth year is:",age,"years old")
+
+# Write a program that calculates the area of a rectangle using length and width variables.
+# Area of recatangle
+print("\nWe are going to calculates the 'Area of a rectangle'. \nBelow:")
+length=float(input("Enter the lenght:"))
+width=int(input("Enter the width:"))
+area=length*width
+print("The area of rectangle is:",area)
+
